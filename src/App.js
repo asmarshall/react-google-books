3e61e37a2c27a977975318aa3d173931d0172250
Angela Marshall <hello@angelasmarshall.com>
2020-12-01 17:20:37 -0700

    Add basic search form

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>Google Book Search</h1>
+        <form>
+          <input
+            type="search"
+            placeholder="harry potter, lord of the rings, etc..."
+          />
+          <button type="submit">Search</button>
+        </form>
+      </section>
     </div>
   );
 }
